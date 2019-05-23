7982dacc989de4f7477d088ecf858aa8a664c6ba
CrisTafRos <cristian.tafolla.rosales@gmail.com>
2019-05-23 10:59:38 -0500

    creditos.php

---
+++ creditos.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title> Creditos </title>
+</head>
+<body>
+
+<table widht="100%" >
+	<tr><strong> CREDITOS</strong> </tr></br></br>
+
+	<tr> Donaji Hernandez Galicia </tr></br>
+	<tr> Alumna de la clase Desarrollo de aplicaciones para web 2.0 </br> </tr>	
+</table>
+</br>
+</br>
+<a href="index.php">Regresar a Inicio</a>
+
+</body>
+</html>
+

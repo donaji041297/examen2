1b3d5123d98bc51c7d492578a8bde5d8102a2475
CrisTafRos <cristian.tafolla.rosales@gmail.com>
2019-05-23 11:04:28 -0500

    precios.php

---
+++ precios.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title> Precios </title>
+</head>
+<body>
+
+<table widht="50%">
+	<th widht="50%">LUGAR <th>	
+	<th> PRECIO <th>
+	
+	<tr>
+		<td widht="20%">Ghandi</td>
+		<td>$310.00 </td>
+	</tr>
+	<tr>
+		<td>Sanborns</td></br>
+		<td> $310.00 </td>
+	</tr>
+</table>	
+
+
+
+</body>
+</html>
+
